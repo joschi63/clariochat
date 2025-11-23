a07156124a9ac3039dfdce979fb541c7742f8d23
Johann Schneider <joschi.dev7@gmail.com>
2025-11-23 08:05:57 +0100

    implmented basic database logic

---
+++ app/database/database.py
@@ -0,0 +1,14 @@
+from sqlmodel import SQLModel, create_engine, Session, Field
+from .models import User
+
+postgres_url = f"postgresql+psycopg://postgres:13579@localhost:5432/clariochat"
+
+engine = create_engine(postgres_url, echo=True)
+
+with Session(engine) as session:
+    SQLModel.metadata.create_all(engine)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from .database.database import create_db_and_tables
+
+create_db_and_tables()
 
 app = FastAPI()
+
 
 @app.get("/")
 def root():
