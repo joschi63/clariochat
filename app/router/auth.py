f50e98c101607aae346dbc242a1576459844d143
Johann Schneider <joschi.dev7@gmail.com>
2025-12-04 19:21:09 +0100

    set up token verification for login of users

--- app/router/auth.py
+++ app/router/auth.py
@@ -1,17 +1,25 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from ..database.models import User
 from ..database.database import SessionDep
 from ..schemas.user_schema import UserCreate, UserRead
 from sqlmodel import Session, select
 from argon2 import PasswordHasher
-from ..security.token_managing import get_current_user
+from ..security.token_managing import create_acces_token, Token
+from datetime import timedelta
 
 router = APIRouter()
 ph = PasswordHasher()
 
 @router.get("/login")
-def login(user: User = Depends(get_current_user), form_data: OAuth2PasswordRequestForm = Depends()):
+def login(session: SessionDep, form_data: OAuth2PasswordRequestForm = Depends()):
+    user = session.exec(select(User).where(User.phone_number == form_data.username)).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password"
+        )
+    
     try:
         ph.verify(user.password, form_data.password)
     except:
@@ -19,4 +27,8 @@ def login(user: User = Depends(get_current_user), form_data: OAuth2PasswordReque
             status_code=400,
             detail="Incorrect username or password"
         )
-    return {"message": f"Welcome {user.name}!"}
+    access_token_expires = timedelta(minutes=30)
+    access_token = create_acces_token(
+        data={"sub": user.phone_number}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
--- app/security/token_managing.py
+++ app/security/token_managing.py
@@ -6,16 +6,47 @@ from ..schemas.user_schema import UserSchema
 from ..database.models import User
 from ..database.database import SessionDep
 from sqlmodel import select
+from pydantic import BaseModel
+from datetime import datetime, timedelta, timezone
+import jwt
+from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+SECRET_KEY = "ab8a2d30666b0952fc50b90d8ba4a7b528ba0d9ade15da0fec8fba684d56474b"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+def create_acces_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_kwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_kwt
+
+    
 def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], session: SessionDep):
-    user = session.exec(select(User).where(User.phone_number == token)).first()
-
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try: 
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        phone_number:str = payload.get("sub")
+        if phone_number is None:
+            raise credentials_exception
+    except InvalidTokenError:
+        raise credentials_exception
+    
+    user = session.exec(select(User).where(User.phone_number == phone_number)).first()
+    if user is None:
+        raise credentials_exception
+    
     return user
